3bf64ae57d6df30549398095c2cfb8e79854fc7f
Michael Branon <michaelabranon@gmail.com>
2016-12-18 04:37:27 -0500

    renamed card test package

--- Test Packages/cards/CardTypes.java
+++ Test Packages/cards/CardTypes.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package card.tests;
+package cards;
 
 import org.junit.Assert;
 import org.junit.Test;

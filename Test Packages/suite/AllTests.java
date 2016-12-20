f60c57b59c4174eec803451ca58984ba03b8ac4a
Michael Branon <michaelabranon@gmail.com>
2016-12-19 23:28:47 -0500

    minor change to test suite

--- Test Packages/suite/AllTests.java
+++ Test Packages/suite/AllTests.java
@@ -5,10 +5,6 @@
  */
 package suite;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
